109c67c0c8d6e75c5e4c5e3a227f9dfbcac0aff7
oksana.babychevska <oksana.babychevska@gmail.com>
2024-11-08 19:07:17 +0100

    add changes

--- app.py
+++ app.py
@@ -1,29 +1,34 @@
-import unittest
-import requests
+import os
+from flask import Flask, render_template, request
+from prometheus_client import start_http_server, Summary, generate_latest
+import time
 
+app = Flask(__name__)
 
-class TestTemperatureApp(unittest.TestCase):
-    BASE_URL = 'http://localhost:5000'  # The base URL for the Flask app
+flask_port = os.getenv('FLASK_PORT', 5000)
+metrics_port = os.getenv('METRICS_PORT', 8000)
 
-    def test_convert_temperature(self):
-        # Sending a POST request to the root route ('/')
-        response = requests.post(f'{self.BASE_URL}/', data={'celsius': 25})
-        
-        # Check if the response code is 200 (OK)
-        self.assertEqual(response.status_code, 200)
-        
-        # Assert that the response contains the expected 'fahrenheit' value
-        self.assertIn('fahrenheit', response.text)  # Checking if the fahrenheit result is in the response
+REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
 
-    def test_invalid_temperature(self):
-        # Sending a POST request with invalid data
-        response = requests.post(f'{self.BASE_URL}/', data={'celsius': 'invalid'})
-        
-        # Check if the response code is 400 (Bad Request)
-        self.assertEqual(response.status_code, 200)  # This should still return a 200 OK with an error message
-        
-        # Ensure the response contains the error message
-        self.assertIn("Invalid input", response.text)
+@REQUEST_TIME.time()
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    fahrenheit = None  
+    celsius = None
+
+    if request.method == 'POST':
+        try:
+            celsius = float(request.form['celsius'])
+            fahrenheit = celsius * 9 / 5 + 32
+        except ValueError:
+            fahrenheit = "Invalid input. Please enter a number."
+
+    return render_template('index.html', celsius=celsius, fahrenheit=fahrenheit)
+
+@app.route('/metrics')
+def metrics():
+    return generate_latest(), 200
 
 if __name__ == '__main__':
-    unittest.main()
+    start_http_server(int(metrics_port)) 
+    app.run(host='0.0.0.0', port=int(flask_port))
