4c8d8433bb4f26c9392066339c090a875dbe77c6
oksana.babychevska <oksana.babychevska@gmail.com>
2024-11-08 20:20:40 +0100

    add changes

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 import os
 from flask import Flask, render_template, request
-from prometheus_client import start_http_server, Summary, generate_latest
+from prometheus_client import Summary, generate_latest, make_wsgi_app
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
 import time
 
+# Ініціалізація Flask додатку
 app = Flask(__name__)
 
+# Порти, на яких буде працювати додаток
 flask_port = os.getenv('FLASK_PORT', 5000)
 metrics_port = os.getenv('METRICS_PORT', 8000)
 
+# Оголошуємо метрику для вимірювання часу обробки запитів
 REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
 
+# Маршрут для головної сторінки
 @REQUEST_TIME.time()
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -25,10 +30,17 @@ def index():
 
     return render_template('index.html', celsius=celsius, fahrenheit=fahrenheit)
 
+# Маршрут для метрик, який використовує Prometheus для збору даних
 @app.route('/metrics')
 def metrics():
     return generate_latest(), 200
 
+# Запуск Flask додатку з прометеусом на одному порті
 if __name__ == '__main__':
-    start_http_server(int(metrics_port)) 
+    # Інтеграція Prometheus метрик як middleware
+    app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
+        '/metrics': make_wsgi_app()  # Всі запити до /metrics будуть оброблятися окремо
+    })
+    
+    # Запуск Flask серверу
     app.run(host='0.0.0.0', port=int(flask_port))
